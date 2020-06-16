48891b82664ae3732cd8885b9e9c6fb962c18a1c
Tomokazu-Kirihara <miw55kzts@icloud.com>
2020-06-16 15:22:37 +0900

    complete

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                  {"きりはら"}
+    email                 {"k@k"}
+    password              {"password0"}
+    password_confirmation {"password0"}
+  end
+end
